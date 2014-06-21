ba0c919b53b5ed967b87863681525bddffa28a58
Mario <augustulus\freenet.de>
2014-06-21 17:31:24 +0200

    class Kunde ,Rechnung hinzugefügt

---
+++ src/main/java/com/mario/blumenladen/Kunde.java
@@ -0,0 +1,19 @@
+
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+ */
+package com.mario.blumenladen;
+
+/**
+ *
+ * @author mario
+ */
+public class Kunde {
+    String name;
+    String anschrift;
+}
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
---
+++ src/main/java/com/mario/blumenladen/Rechnung.java
@@ -0,0 +1,21 @@
+
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+ */
+package com.mario.blumenladen;
+
+/**
+ *
+ * @author mario
+ */
+public class Rechnung {
+    double betrag = 0;
+    double mehrwertsteuer;
+    double rabatt;
+    Kunde  rechnungsempfänger;
+}
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
