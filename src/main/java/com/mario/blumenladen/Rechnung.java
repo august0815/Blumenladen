66e38e806035550c9414f9259550802af90144a4
Mario <augustulus\freenet.de>
2014-06-21 17:42:42 +0200

    leere Methoden hinzugefügt

--- src/main/java/com/mario/blumenladen/Rechnung.java
+++ src/main/java/com/mario/blumenladen/Rechnung.java
@@ -15,6 +15,36 @@ public class Rechnung {
     double mehrwertsteuer;
     double rabatt;
     Kunde  rechnungsempfänger;
+
+    void gebeAus() {
+        double netto  = betrag * (1 - rabatt);
+        double brutto = netto * (1 + mehrwertsteuer);
+
+        System.out.println("An:");
+        System.out.println(rechnungsempfänger.name);
+        System.out.println(rechnungsempfänger.anschrift);
+        System.out.println("Netto : ");
+        System.out.println(netto);
+        System.out.println("MwSt: ");
+        System.out.println(mehrwertsteuer);
+        System.out.println("Brutto: ");
+        System.out.println(brutto);
+    }
+
+    void legeRabattFest(double neuerRabatt) {
+
+        // TODO
+    }
+
+    void legeMehwertsteuerFest(double neueMwSt) {
+
+        // TODO
+    }
+
+    void legePostenHinzu(int anzahl, double einzelpreis) {
+
+        // TODO
+    }
 }
 
 
